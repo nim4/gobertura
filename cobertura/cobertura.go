2dae5dcf0f3ea0e00acfbd8f250dc615538ddff9
nim4 <ghotbi.nima@gmail.com>
2020-11-09 03:20:29 +0100

    fix coverage

--- cobertura/cobertura.go
+++ cobertura/cobertura.go
@@ -33,25 +33,21 @@ type Source struct {
 }
 
 type Package struct {
-	numLines         int64
-	numLinesWithHits int64
-	Name             string   `xml:"name,attr"`
-	LineRate         float32  `xml:"line-rate,attr"`
-	BranchRate       float32  `xml:"branch-rate,attr"`
-	Complexity       float32  `xml:"complexity,attr"`
-	Classes          []*Class `xml:"classes>class"`
+	Name       string   `xml:"name,attr"`
+	LineRate   float32  `xml:"line-rate,attr"`
+	BranchRate float32  `xml:"branch-rate,attr"`
+	Complexity float32  `xml:"complexity,attr"`
+	Classes    []*Class `xml:"classes>class"`
 }
 
 type Class struct {
-	numLines         int64
-	numLinesWithHits int64
-	Name             string    `xml:"name,attr"`
-	Filename         string    `xml:"filename,attr"`
-	LineRate         float32   `xml:"line-rate,attr"`
-	BranchRate       float32   `xml:"branch-rate,attr"`
-	Complexity       float32   `xml:"complexity,attr"`
-	Methods          []*Method `xml:"methods>method"`
-	Lines            Lines     `xml:"lines>line"`
+	Name       string    `xml:"name,attr"`
+	Filename   string    `xml:"filename,attr"`
+	LineRate   float32   `xml:"line-rate,attr"`
+	BranchRate float32   `xml:"branch-rate,attr"`
+	Complexity float32   `xml:"complexity,attr"`
+	Methods    []*Method `xml:"methods>method"`
+	Lines      Lines     `xml:"lines>line"`
 }
 
 type Method struct {
@@ -69,23 +65,35 @@ type Line struct {
 }
 
 // Lines is a slice of Line pointers, with some convenience methods
-type Lines struct {
-	numLinesWithHits int64
-	inner []*Line
-}
+type Lines []*Line
 
 // HitRate returns a float32 from 0.0 to 1.0 representing what fraction of lines
 // have hits
 func (lines Lines) HitRate() (hitRate float32) {
-	return float32(lines.numLinesWithHits) / float32(len(lines.inner))
+	return float32(lines.NumLinesWithHits()) / float32(len(lines))
+}
+
+// NumLines returns the number of lines
+func (lines Lines) NumLines() int64 {
+	return int64(len(lines))
+}
+
+// NumLinesWithHits returns the number of lines with a hit count > 0
+func (lines Lines) NumLinesWithHits() (numLinesWithHits int64) {
+	for _, line := range lines {
+		if line.Hits > 0 {
+			numLinesWithHits++
+		}
+	}
+	return numLinesWithHits
 }
 
 // AddOrUpdateLine adds a line if it is a different line than the last line recorded.
 // If it's the same line as the last line recorded then we update the hits down
 // if the new hits is less; otherwise just leave it as-is
 func (lines *Lines) AddOrUpdateLine(lineNumber int, hits int64) {
-	if len(lines.inner) > 0 {
-		lastLine := lines.inner[len(lines.inner)-1]
+	if len(*lines) > 0 {
+		lastLine := (*lines)[len(*lines)-1]
 		if lineNumber == lastLine.Number {
 			if hits < lastLine.Hits {
 				lastLine.Hits = hits
@@ -93,7 +101,89 @@ func (lines *Lines) AddOrUpdateLine(lineNumber int, hits int64) {
 			return
 		}
 	}
-	lines.inner = append(lines.inner, &Line{Number: lineNumber, Hits: hits})
+	*lines = append(*lines, &Line{Number: lineNumber, Hits: hits})
+}
+
+// HitRate returns a float32 from 0.0 to 1.0 representing what fraction of lines
+// have hits
+func (method Method) HitRate() float32 {
+	return method.Lines.HitRate()
+}
+
+// NumLines returns the number of lines
+func (method Method) NumLines() int64 {
+	return method.Lines.NumLines()
+}
+
+// NumLinesWithHits returns the number of lines with a hit count > 0
+func (method Method) NumLinesWithHits() int64 {
+	return method.Lines.NumLinesWithHits()
+}
+
+// HitRate returns a float32 from 0.0 to 1.0 representing what fraction of lines
+// have hits
+func (class Class) HitRate() float32 {
+	return float32(class.NumLinesWithHits()) / float32(class.NumLines())
+}
+
+// NumLines returns the number of lines
+func (class Class) NumLines() (numLines int64) {
+	for _, method := range class.Methods {
+		numLines += method.NumLines()
+	}
+	return numLines
+}
+
+// NumLinesWithHits returns the number of lines with a hit count > 0
+func (class Class) NumLinesWithHits() (numLinesWithHits int64) {
+	for _, method := range class.Methods {
+		numLinesWithHits += method.NumLinesWithHits()
+	}
+	return numLinesWithHits
+}
+
+// HitRate returns a float32 from 0.0 to 1.0 representing what fraction of lines
+// have hits
+func (pkg Package) HitRate() float32 {
+	return float32(pkg.NumLinesWithHits()) / float32(pkg.NumLines())
+}
+
+// NumLines returns the number of lines
+func (pkg Package) NumLines() (numLines int64) {
+	for _, class := range pkg.Classes {
+		numLines += class.NumLines()
+	}
+	return numLines
+}
+
+// NumLinesWithHits returns the number of lines with a hit count > 0
+func (pkg Package) NumLinesWithHits() (numLinesWithHits int64) {
+	for _, class := range pkg.Classes {
+		numLinesWithHits += class.NumLinesWithHits()
+	}
+	return numLinesWithHits
+}
+
+// HitRate returns a float32 from 0.0 to 1.0 representing what fraction of lines
+// have hits
+func (cov Coverage) HitRate() float32 {
+	return float32(cov.NumLinesWithHits()) / float32(cov.NumLines())
+}
+
+// NumLines returns the number of lines
+func (cov Coverage) NumLines() (numLines int64) {
+	for _, pkg := range cov.Packages {
+		numLines += pkg.NumLines()
+	}
+	return numLines
+}
+
+// NumLinesWithHits returns the number of lines with a hit count > 0
+func (cov Coverage) NumLinesWithHits() (numLinesWithHits int64) {
+	for _, pkg := range cov.Packages {
+		numLinesWithHits += pkg.NumLinesWithHits()
+	}
+	return numLinesWithHits
 }
 
 func (cov *Coverage) ParseProfiles(profiles []*cover.Profile) error {
@@ -105,30 +195,9 @@ func (cov *Coverage) ParseProfiles(profiles []*cover.Profile) error {
 		}
 	}
 
-	numLines := int64(0)
-	numLinesWithHits := int64(0)
-	for _, pkg := range cov.Packages {
-		for _, class := range pkg.Classes {
-			for _, method := range class.Methods {
-				for _, line := range method.Lines.inner {
-					if line.Hits > 0 {
-						method.Lines.numLinesWithHits++
-					}
-				}
-				class.numLines += int64(len(method.Lines.inner))
-				class.numLinesWithHits += method.Lines.numLinesWithHits
-			}
-			pkg.numLines += class.numLines
-			pkg.numLinesWithHits += class.numLinesWithHits
-		}
-
-		numLines += pkg.numLines
-		numLines += pkg.numLinesWithHits
-	}
-
-	cov.LinesValid = numLines
-	cov.LinesCovered = numLinesWithHits
-	cov.LineRate = float32(numLinesWithHits) / float32(numLines)
+	cov.LinesValid = cov.NumLines()
+	cov.LinesCovered = cov.NumLinesWithHits()
+	cov.LineRate = cov.HitRate()
 	return nil
 }
 
@@ -167,7 +236,7 @@ func (cov *Coverage) parseProfile(profile *cover.Profile) error {
 		profile:  profile,
 	}
 	ast.Walk(visitor, parsed)
-	pkg.LineRate = float32(pkg.numLinesWithHits) / float32(pkg.numLines)
+	pkg.LineRate = pkg.HitRate()
 	return nil
 }
 
@@ -187,7 +256,9 @@ func (v *fileVisitor) Visit(node ast.Node) ast.Visitor {
 		method := v.method(n)
 		method.LineRate = method.Lines.HitRate()
 		class.Methods = append(class.Methods, method)
-		class.Lines.inner = append(class.Lines.inner, method.Lines.inner...)
+		for _, line := range method.Lines {
+			class.Lines = append(class.Lines, line)
+		}
 		class.LineRate = class.Lines.HitRate()
 	}
 	return v
